11420f5a2e98e5ef5a677f80d248d722b1c2ee80
Asger Asgerov <asgaraa@code.edu.az>
2022-04-12 12:23:59 +0400

    TeacherSkillStart

--- EduHome/EduHome/Controllers/TeacherController.cs
+++ EduHome/EduHome/Controllers/TeacherController.cs
@@ -1,7 +1,5 @@
 ﻿using EduHome.Data;
-using EduHome.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +7,23 @@ using System.Threading.Tasks;
 
 namespace EduHome.Controllers
 {
-    public class BlogController : Controller
+    public class TeacherController : Controller
     {
         private readonly AppDbContext _context;
-        public BlogController(AppDbContext context)
+        public TeacherController(AppDbContext context)
         {
             _context = context;
         }
         public async Task<IActionResult> Index()
         {
-            //List<Blog> blogs =
+           
             return View();
         }
 
         public async Task<IActionResult> Detail(int Id)
         {
-            Blog blog = await _context.Blogs.Where(m => m.Id == Id).FirstOrDefaultAsync();
-            return View(blog);
+            //Blog blog = await _context.Blogs.Where(m => m.Id == Id).FirstOrDefaultAsync();
+            return View();
         }
     }
 }
